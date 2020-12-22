1b40498781a7866f2fcb11b8e2143d6366221e97
lihui47 <1660991547.com>
2020-12-22 19:20:32 +0800

    12.22lh

---
+++ src/main/java/com/woniu/vo/UserBlurVo.java
@@ -0,0 +1,9 @@
+package com.woniu.vo;
+
+import lombok.Data;
+
+@Data
+public class UserBlurVo {
+    private String input3;
+    private String select;
+}
---
+++ src/main/java/com/woniu/vo/UserPageVo.java
@@ -0,0 +1,9 @@
+package com.woniu.vo;
+
+import lombok.Data;
+
+@Data
+public class UserPageVo {
+    private Integer current;
+    private Integer size;
+}
