ead97e399c434244da9d421ad1dab54501a88cfb
sxj1991 <sxj1991>
2020-12-23 22:52:17 +0800

    Fitst UPload

---
+++ src/main/java/com/woniu/domin/ProductAttribute.java
@@ -0,0 +1,58 @@
+package com.woniu.domin;
+
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName ProductAttribute
+ * @Description TODO
+ * @Authro SheepSun
+ * @Date 2020/12/23 16:03
+ * @Version 1.0
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_product")
+@ApiModel(value = "ProductAttribute对象", description = "")
+@Component
+public class ProductAttribute implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "商品id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private Integer sid;
+
+    private Integer number;
+
+    private double price;
+
+    private Integer status;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModifified;
+
+    @TableField(exist = false)
+    private String statusName;
+
+    @TableField(exist = false)
+    private String secondName;
+}
---
+++ src/main/java/com/woniu/service/ProductAttributeService.java
@@ -0,0 +1,28 @@
+package com.woniu.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.woniu.domin.ProductAttribute;
+import com.woniu.dto.Result;
+import com.woniu.vo.CheckVO;
+import com.woniu.vo.PageVO;
+
+
+public interface ProductAttributeService extends IService<ProductAttribute> {
+    //查询二级商品小类
+    Page queryProductAttrAllInfo(PageVO pageVO) throws Exception;
+
+    //模糊查询商品大类
+    Page queryProductAttrLikeInfo(CheckVO checkVO) throws Exception;
+
+    //新增商品信息
+    Result insertProductAttrInfo(ProductAttribute productAttribute) throws Exception;
+
+    //修改商品信息
+    Result updataProductAttrInfo(ProductAttribute productAttribute) throws Exception;
+
+    //删除商品信息
+    Result deleteProductAttrInfo(ProductAttribute productAttribute) throws Exception;
+
+
+}
