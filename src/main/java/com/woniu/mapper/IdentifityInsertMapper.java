018a68cfb54f0ef81958b07c8f8a6260e49a7075
sxj1991 <sxj1991>
2020-12-24 13:33:18 +0800

    Fitst UPload

---
+++ src/main/java/com/woniu/mapper/IdentifityInsertMapper.java
@@ -0,0 +1,7 @@
+package com.woniu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.woniu.domin.Identifity;
+
+public interface IdentifityInsertMapper extends BaseMapper<Identifity> {
+}
--- src/main/java/com/woniu/service/impl/ProductAttrbuteImpl.java
+++ src/main/java/com/woniu/service/impl/ProductAttrbuteImpl.java
@@ -4,11 +4,12 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
+import com.woniu.domin.Identifity;
 import com.woniu.domin.ProductAttribute;
 import com.woniu.domin.Second;
 import com.woniu.dto.Result;
 import com.woniu.dto.StatusCode;
+import com.woniu.mapper.IdentifityInsertMapper;
 import com.woniu.mapper.ProductAttrVOMapper;
 import com.woniu.mapper.ProductAttributeMapper;
 import com.woniu.mapper.SecondMapper;
@@ -16,6 +17,8 @@ import com.woniu.service.ProductAttributeService;
 import com.woniu.vo.CheckVO;
 import com.woniu.vo.PageVO;
 import com.woniu.vo.ProductAttrVO;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,18 +42,31 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
     private ProductAttrVOMapper productAttrVOMapper;
     @Resource
     private SecondMapper secondMapper;
+    @Resource
+    private RedisTemplate<Object, Object> redis;
+    @Resource
+    private IdentifityInsertMapper identifityInsertMapper;
+
+    @Resource
+    private Identifity identifity;
 
     @Override
     @Transactional(propagation = Propagation.SUPPORTS,readOnly = true)
     public Page queryProductAttrAllInfo(PageVO pageVO) throws Exception {
+
         Page<ProductAttrVO> page = new Page<>(pageVO.getCurrent(),pageVO.getSizePage());
-        productAttrVOMapper.productAttrPageInfo(page);
-        List<ProductAttrVO> records = page.getRecords();
-        for (ProductAttrVO record : records) {
-            if(record.getStatus()==0){
-                record.setStatusName("未鉴定");
+        //设置redis缓存
+        ListOperations<Object, Object> redisList = redis.opsForList();
+
+            System.out.println("从数据库查询");
+            productAttrVOMapper.productAttrPageInfo(page);
+            List<ProductAttrVO> records = page.getRecords();
+            for (ProductAttrVO record : records) {
+                //redis存储
+                if(record.getStatus().equals("未鉴定")){
+                    record.setStatusName("未鉴定");
+                }
             }
-        }
         return page;
     }
 
@@ -63,7 +79,7 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
         productAttrVOMapper.productAttrLikePage(page, queryWrapper);
         List<ProductAttrVO> records = page.getRecords();
         for (ProductAttrVO record : records) {
-            if(record.getStatus()==0){
+            if(record.getStatus().equals("未鉴定")){
                 record.setStatusName("未鉴定");
             }
         }
@@ -75,7 +91,7 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
     public Result insertProductAttrInfo(ProductAttribute productAttribute) throws Exception {
         if (!ObjectUtils.isEmpty(productAttribute)){
             if(productAttribute.getStatusName().equals("未鉴定")){
-                productAttribute.setStatus(0);
+                productAttribute.setStatus("未鉴定");
             }
             boolean b =true;
             List<ProductAttribute> productAttributes = productAttributeMapper.selectList(null);
@@ -90,6 +106,9 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
             if(b&&!ObjectUtils.isEmpty(second)){
                 productAttribute.setSid(second.getId());
                 productAttributeMapper.insert(productAttribute);
+                //设置商品列表的操作人
+                identifity.setPid(productAttribute.getId()).setWriter(productAttribute.getUserName());
+                identifityInsertMapper.insert(identifity);
                 return new Result(true, StatusCode.OK,"新增成功");
             }
         }
@@ -107,14 +126,12 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
                 QueryWrapper<ProductAttribute> updateWrapper = new QueryWrapper<>();
                 updateWrapper.eq("id",productAttribute.getId());
                 if(productAttribute.getStatusName().equals("未鉴定")){
-                    productAttribute.setStatus(0);
+                    productAttribute.setStatus("未鉴定");
                     productAttributeMapper.update(productAttribute, updateWrapper);
                     return new Result(true,StatusCode.OK,"修改成功");
                 }
             }
         }
-
-
         return null;
     }
 
@@ -129,12 +146,11 @@ public class ProductAttrbuteImpl extends ServiceImpl<ProductAttributeMapper, Pro
         }
         return null;
     }
-    /*
-    鉴定完成后，修改商品状态
-     */
+
     @Override
     public int updateProductStatus(ProductAttribute productAttribute) {
-        productAttributeMapper.updateById(productAttribute);
         return 0;
     }
+
+
 }
